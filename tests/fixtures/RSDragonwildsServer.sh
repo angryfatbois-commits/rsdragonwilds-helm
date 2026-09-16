ec0e53c67a129762b53d43b4321c14ada9a6a36a
Dylan Petzko <petzkod@gmail.com>
2026-09-16 04:29:21 -0400

    test: preserve exact downloaded launcher quoting

--- tests/fixtures/RSDragonwildsServer.sh
+++ tests/fixtures/RSDragonwildsServer.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-UE_TRUE_SCRIPT_NAME=$(echo "$0" | xargs readlink -f)
+UE_TRUE_SCRIPT_NAME=$(echo \"$0\" | xargs readlink -f)
 UE_PROJECT_ROOT=$(dirname "$UE_TRUE_SCRIPT_NAME")
 chmod +x "$UE_PROJECT_ROOT/RSDragonwilds/Binaries/Linux/RSDragonwildsServer-Linux-Shipping"
 "$UE_PROJECT_ROOT/RSDragonwilds/Binaries/Linux/RSDragonwildsServer-Linux-Shipping" RSDragonwilds "$@" 
